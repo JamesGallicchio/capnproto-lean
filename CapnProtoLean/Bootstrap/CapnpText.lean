4aab2b064f5d411647742f1b13e842b53a83d4c4
James <jamesgallicchio@gmail.com>
2024-08-01 13:57:48 -0400

    add back stuff that was there before refactor

--- CapnProtoLean/Bootstrap/CapnpText.lean
+++ CapnProtoLean/Bootstrap/CapnpText.lean
@@ -1,5 +1,7 @@
 import Batteries
 
+import CapnProtoLean.Schema
+
 namespace CapnProtoLean
 
 deriving instance Repr for Batteries.AssocList
@@ -102,3 +104,137 @@ def id? (c : CapnpText) : Except String String := do
     pure s
   | _ =>
     throw s!"id expected:\n{repr c}"
+
+end CapnpText
+
+def CodeGeneratorRequest.fromText (t : CapnpText) : Except String CodeGeneratorRequest := do
+  let t ← t.struct? "codegeneratorrequest"
+  let capnpVersion ← (do
+    let t ← t.field "capnpVersion" |>.struct? "capnpversion"
+    let major := (← t.field "major" |>.num?).toUInt16
+    let minor := (← t.field "minor" |>.num?).toUInt8
+    let micro := (← t.field "micro" |>.num?).toUInt8
+    return some { major, minor, micro })
+  let nodes ← (do
+    let ts ← t.field "nodes" |>.arr?
+    ts.mapM (fun t => do
+      let t ← t.struct? "node"
+      let id := (← (t.field "id").num?).toUInt64
+      let displayName ← (t.field "displayName").str?
+      let displayNamePrefixLength := (← (t.field "displayNamePrefixLength").num?).toUInt32
+      let scopeId := (← (t.field "scopeId").num?).toUInt64
+      let parameters ← (do
+        let ts ← t.field "parameters" |>.arr?
+        ts.mapM fun t => do
+          let t ← t.struct? "parameter"
+          let name ← (t.field "name").str?
+          return .mk name)
+      let isGeneric ←
+        match (← (t.field "isGeneric").id?) with
+        | "true" => pure true
+        | "false" => pure false
+        | s => throw s!"expected bool, got {s}"
+      let nestedNodes ← (do
+        let t ← (t.field "nestedNodes").arr?
+        return #[])
+      let annotations ← (do
+        let t ← (t.field "annotations").arr?
+        return #[])
+      let body ← (do
+        try
+          let t ← t.field? "file"
+          return .file
+        catch _ =>
+        try
+          let t ← t.field? "struct"
+          let t ← t.struct? "node struct group"
+          let dataWordCount := (← t.field "dataWordCount" |>.num?).toUInt16
+          let pointerCount := (← t.field "pointerCount" |>.num?).toUInt16
+          let preferredListEncoding := default
+          let isGroup := default
+          let discriminantCount := (← t.field "discriminantCount" |>.num?).toUInt16
+          let discriminantOffset := (← t.field "discriminantOffset" |>.num?).toUInt32
+          let fields := default
+          return Node.Body.struct dataWordCount pointerCount preferredListEncoding
+            isGroup discriminantCount discriminantOffset fields
+        catch _ =>
+        try
+          let t ← t.field? "enum"
+          let enumerants := default
+          return .enum enumerants
+        catch _ =>
+        try
+          let t ← t.field? "interface"
+          let methods := default
+          let superclasses := default
+          return .interface methods superclasses
+        catch _ =>
+        try
+          let t ← t.field? "const"
+          let t ← t.struct? "node const group"
+          let type := «Type».mk «Type».Body.text
+          let value := Value.mk («Value».Body.text "")
+          return .const type value
+        catch _ =>
+        try
+          let t ← t.field? "annotation"
+          let type := «Type».mk «Type».Body.text
+          let targetsFile := false
+          let targetsConst := false
+          let targetsEnum := false
+          let targetsEnumerant := false
+          let targetsStruct := false
+          let targetsField := false
+          let targetsUnion := false
+          let targetsGroup := false
+          let targetsInterface := false
+          let targetsMethod := false
+          let targetsParam := false
+          let targetsAnnotation := false
+          return .annotation type
+            targetsFile targetsConst targetsEnum targetsEnumerant targetsStruct
+            targetsField targetsUnion targetsGroup targetsInterface targetsMethod
+            targetsParam targetsAnnotation
+        catch _ =>
+          throw s!"struct union had no field selected:\n{repr t}")
+      return .mk
+        id displayName displayNamePrefixLength scopeId parameters
+        isGeneric nestedNodes annotations body
+      ))
+  let sourceInfo ← (do
+    let ts ← t.field "sourceInfo" |>.arr?
+    ts.mapM (fun t => do
+      let t ← t.struct? "sourceinfo"
+      let id := (← (t.field "id").num?).toUInt64
+      let docComment := (← (t.field "docComment").str?)
+      let members ← (do
+        let ts ← t.field "members" |>.arr?
+        ts.mapM (fun t => do
+          let t ← t.struct? "member"
+          let docComment := (← t.field "docComment" |>.str?)
+          return .mk
+            docComment))
+      return .mk
+        id docComment members))
+  let requestedFiles ← (do
+    let ts ← t.field "requestedFiles" |>.arr?
+    ts.mapM (fun t => do
+      let t ← t.struct? "requestedfile"
+      let id := (← (t.field "id").num?).toUInt64
+      let filename := (← (t.field "filename").str?)
+      let imports ← (do
+        let ts ← (t.field "imports").arr?
+        ts.mapM (fun t => do
+          let t ← t.struct? "import"
+          let id := (← (t.field "id").num?).toUInt64
+          let name := (← (t.field "name").str?)
+          return .mk
+            id name))
+      return .mk
+        id filename imports))
+  return {
+    capnpVersion
+    nodes
+    sourceInfo
+    requestedFiles
+  }
