41c83d97d6bfda0c28d9a25b7e572bdb394389c6
James <jamesgallicchio@gmail.com>
2024-08-01 15:38:02 -0400

    run_elab_cached command

--- CapnProtoLean/Utils/Meta.lean
+++ CapnProtoLean/Utils/Meta.lean
@@ -1,4 +1,5 @@
 import Batteries
+import Qq
 
 namespace CapnProtoLean.Utils
 
@@ -29,8 +30,86 @@ deriving Inhabited, Repr
 
 declare_view Hi Hi.View
 
-/-- info: { blah := 0 } -/
-#guard_msgs in
-#eval Hi.ofView ⟨0⟩ |>.view
+end Example
+
+scoped syntax (name := run_elab_cached)
+  "#run_elab_cached " term:max " {{" (ppLine command)* ppDedent(ppLine "}}") : command
+
+deriving instance TypeName for String
+
+open Qq Meta
+
+@[command_elab run_elab_cached]
+def runElabCachedHandler : CommandElab := fun stx => do
+  match stx with
+  | `(command| #run_elab_cached $e:term {{ $c:command* }} ) =>
+    let cmds ← termToCmds e
+    if cmds != c then
+      logWarning "The macro output differs from the syntax between {{ }}"
+      let fmt ← format e cmds
+      pushInfoLeaf (.ofCustomInfo {
+        stx := ← getRef,
+        value := Dynamic.mk fmt.pretty })
+    for c in c do elabCommand c
+  | _ =>
+    throwUnsupportedSyntax
+where
+  termToCmds (e : Term) : CommandElabM (TSyntaxArray `command) := do
+    let elabM : CommandElabM (TSyntaxArray `command) ← liftTermElabM <| do
+        let expr ← Term.elabTerm e (some q(CommandElabM (TSyntaxArray `command)))
+        unsafe evalExpr (CommandElabM (TSyntaxArray `command))
+                      (q(CommandElabM (TSyntaxArray `command)))
+                      expr
+    elabM
+  format (e : Term) (cmds : TSyntaxArray `command) : CommandElabM Format := do
+    let syn ← `(command|
+      #run_elab_cached $e {{
+        $cmds:command*
+      }}
+    )
+    let parenthesized ← liftCoreM <| Lean.PrettyPrinter.parenthesizeCommand syn
+    return ← liftCoreM <| Lean.PrettyPrinter.formatCommand parenthesized
+
+open Server RequestM CodeAction in
+@[command_code_action run_elab_cached]
+def runElabCachedCodeAction : CommandCodeAction := fun _ _ _ node => do
+  let .node _ ts := node | return #[]
+  let res := ts.findSome? fun
+    | .node (.ofCustomInfo { stx, value }) _ => return (stx, (← value.get? String))
+    | _ => none
+  let some (stx, newText) := res | return #[]
+  let eager := {
+    title := "Update #run_elab_cached with correct output"
+    kind? := "quickfix"
+    isPreferred? := true
+  }
+  let doc ← readDoc
+  pure #[{
+    eager
+    lazy? := some do
+      let some start := stx.getPos? true | return eager
+      let some tail := stx.getTailPos? true | return eager
+      pure {
+        eager with
+        edit? := some <| .ofTextEdit doc.versionedIdentifier {
+          range := doc.meta.text.utf8RangeToLspRange ⟨start, tail⟩
+          newText
+        }
+      }
+  }]
+
+namespace Example
+
+def hiCmd : CommandElabM (TSyntaxArray `command) := do
+  return #[
+    ← `(command|
+      def $(mkIdent `hi) : $(mkIdent `String) := "hello world"
+    )
+  ]
+
+#run_elab_cached hiCmd {{
+  def hi : String :=
+    "hello world"
+}}
 
 end Example
